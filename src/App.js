5615eba2c359ac0ab823fb42acd84e6a77692918
Thaddaeus <thaddaeusotieno@gmail.com>
2023-03-03 09:56:17 +0300

    xss-started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Main from  './Main'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yaaaah</h1>   
+      <Main />
+    </div>
+  );
+}
+
+export default App;
